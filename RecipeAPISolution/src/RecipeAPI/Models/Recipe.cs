9709e1bb0e8a1add76c892d1f6936f5da299f8aa
joshcv2112 <joshcv2112@gmail.com>
2020-07-01 20:18:12 -0600

    created Recipe model

---
+++ RecipeAPISolution/src/RecipeAPI/Models/Recipe.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipeAPI.Models
+{
+    public class Recipe
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Title { get; set; }
+
+        [Required]
+        public List<string> Ingredients { get; set; }
+
+        [Required]
+        public string Instructions { get; set; }
+
+        public string Description { get; set; }
+
+        public string Source { get; set; }
+
+        public int rating { get; set; }
+
+        public string PrepTime { get; set; }
+        //image?
+        //comments (probably an object of its own)
+    }
+}
