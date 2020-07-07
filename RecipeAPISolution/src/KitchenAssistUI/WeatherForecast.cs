007ecedb6e1aab12d7d6b8ab489a55985444ff16
joshcv2112 <joshcv2112@gmail.com>
2020-07-06 22:04:54 -0600

    initial setup of UI

---
+++ RecipeAPISolution/src/KitchenAssistUI/WeatherForecast.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace KitchenAssistUI
+{
+    public class WeatherForecast
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Ingredients { get; set; }
+
+        public string Instructions { get; set; }
+
+        public string Description { get; set; }
+
+        public string Source { get; set; }
+
+        public int Rating { get; set; }
+
+        public string PrepTime { get; set; }
+
+
+
+
+
+        // OLD
+        // public DateTime Date { get; set; }
+
+        // public int TemperatureC { get; set; }
+
+        // public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        // public string Summary { get; set; }
+    }
+}
