dd9e6b188ba4edf928c3ae6feb7ce2aaeb041cae
joshcv2112 <joshcv2112@gmail.com>
2020-07-23 21:46:50 -0700

    first attempt at release

--- RecipeAPISolution/src/RecipeAPI/Startup.cs
+++ RecipeAPISolution/src/RecipeAPI/Startup.cs
@@ -49,8 +49,9 @@ namespace RecipeAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RecipeContext context)
         {
+            context.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
